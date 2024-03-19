ea081ea17b57dd0ff02abb06618b0b07b4ab921a
candelaccs <143575413+candelaccs@users.noreply.github.com>
2024-03-19 11:17:21 -0300

    Update ejercicio2.py

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,11 +1,4 @@
-#suma = 0
 
-# Iterar sobre los primeros 100 números naturales usando un bucle for
-#for i in range(1, 101):
- #   suma += i  # Sumar cada número a la variable suma
-
-# Imprimir el resultado de la suma
-#print("La suma de los primeros 100 números naturales es:", suma)
 
 nums = 101 
 total = 0
